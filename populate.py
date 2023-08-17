cf1eb54ea13234c2c0ed0ad46f4ced62b8402480
EricKugel <erickugel713@gmail.com>
2023-08-16 22:29:15 -0500

    Error handling is Bad now.

--- populate.py
+++ populate.py
@@ -21,29 +21,26 @@ def create_process():
 
 def populate(schedule):
     try:
-        try:
-            process = create_process()
-            gc = process.result()
-        except TimeoutError:
-            return "Whoops!"
-        
-        events = list(gc.get_events())
-        all_start_times = []
-        for event in events:
-            start = event.start
-            if isinstance(start, datetime):
-                all_start_times.append((start.year, start.month, start.day, start.hour, start.minute))
+        process = create_process()
+        gc = process.result()
+    except TimeoutError:
+        return "Whoops!"
+    
+    events = list(gc.get_events())
+    all_start_times = []
+    for event in events:
+        start = event.start
+        if isinstance(start, datetime):
+            all_start_times.append((start.year, start.month, start.day, start.hour, start.minute))
 
-        for item in schedule:
-            month, day, year = [int(thing) for thing in item[0].split("/")]
-            time_start, time_end = item[1].split(" - ")
-            time_start_hour, time_start_minute = [int(thing) for thing in time_start.split(":")]
-            time_end_hour, time_end_minute = [int(thing) for thing in time_end.split(":")]
-            start = datetime(year, month, day, time_start_hour, time_start_minute)
-            if start < datetime.now():
-                continue
-            if not (year, month, day, time_start_hour, time_start_minute) in all_start_times:
-                event = Event("Eric Working (at Michael's)", start = datetime(year, month, day, time_start_hour, time_start_minute), end = datetime(year, month, day, time_end_hour, time_end_minute), reminders = [PopupReminder(minutes_before_start=60), PopupReminder(minutes_before_start=20)])
-                gc.add_event(event)
-    except:
-        return "Whoopsy daisies!"
+    for item in schedule:
+        month, day, year = [int(thing) for thing in item[0].split("/")]
+        time_start, time_end = item[1].split(" - ")
+        time_start_hour, time_start_minute = [int(thing) for thing in time_start.split(":")]
+        time_end_hour, time_end_minute = [int(thing) for thing in time_end.split(":")]
+        start = datetime(year, month, day, time_start_hour, time_start_minute)
+        if start < datetime.now():
+            continue
+        if not (year, month, day, time_start_hour, time_start_minute) in all_start_times:
+            event = Event("Eric Working (at Michael's)", start = datetime(year, month, day, time_start_hour, time_start_minute), end = datetime(year, month, day, time_end_hour, time_end_minute), reminders = [PopupReminder(minutes_before_start=60), PopupReminder(minutes_before_start=20)])
+            gc.add_event(event)
